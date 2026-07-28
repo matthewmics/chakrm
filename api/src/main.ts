e916f270080bd53fa3dfae860e38d0f4613e391d
Matthew <matthewmics77@gmail.com>
2026-07-28 22:36:38 +0800

    Add logging to AppService and main bootstrap process

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConsoleLogger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new ConsoleLogger({
+    json: true,
+    colors: false,
+  });
+  const app = await NestFactory.create(AppModule, {
+    logger,
+  });
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  logger.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
